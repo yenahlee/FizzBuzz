f46107ada645dbb63311d12c59e7c10ed2ba672f
Yenah Lee <yenah.lee27@gmail.com>
2023-09-11 14:30:15 -0400

    Completed Reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int steps = 0;
+
+        while (i != 0) {
+            if (i % 2 == 0) {
+                i = i / 2;
+                steps++;
+            }
+            else if (i % 2 == 1) {
+                i -= 1;
+                steps++;
+            }
+        }
+
+        System.out.println(steps);
+    }
 }
